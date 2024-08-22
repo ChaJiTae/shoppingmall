a6f081f2581b028651ad498bb2c48c5ee2b10cf3
차지태 <wlxo402@naver.com>
2024-08-23 03:33:24 +0900

    Fix Code Fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import ProductAll from "./pages/ProductAll";
 import Login from "./pages/Login";
-import ProductDetail from "./pages/ProductDetail";
+// import ProductDetail from "./pages/ProductDetail";
 import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
